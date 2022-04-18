2a5127d546312c09e08016c76aedcd2b14978090
Bibek-Bohora <bibchhetri5678@gmail.com>
2022-04-18 16:01:01 +0545

    hello 1time update

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express =require("express");
+const app= express();
+const path=require("path")
+const hbs=require("hbs")
+
+
+// const staticspath= path.join(__dirname,"../views")
+// console.log(staticspath)
+const changecspath= path.join(__dirname,"../server/components")
+console.log(changecspath)
+
+const partialspath= path.join(__dirname,"../server/layout")
+console.log(partialspath)
+
+app.set('view engine',"hbs")
+app.set("views", changecspath)
+
+hbs.registerPartials(partialspath)
+app.get("/",(req,res)=>{
+    res.render("index.hbs",{
+        name:"asim",
+    })
+})
+
+app.get("/about",(req,res)=>{
+    res.render("about.hbs",{
+        status:"logout"
+    })
+
+
+
+})
+
+
+
+
+app.get("/contacts",(req,res)=>{
+    res.render("contacts",{
+        status:"logout"
+    })
+})
+
+
+app.listen(8000,()=>{
+    console.log("connections success")
+})
