00ba4ca34dbfc274bcb2bff82df975eac4e76fa8
Bibek-Bohora <bibchhetri5678@gmail.com>
2022-04-20 13:29:40 +0545

    all update

--- src/app.js
+++ src/app.js
@@ -1,48 +1,40 @@
-const express =require("express");
-const app= express();
-const path=require("path")
-const hbs=require("hbs")
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
 
-const port =process.env.PORT || 8000;
-
+const port = process.env.PORT || 8000;
 
 // const staticspath= path.join(__dirname,"../views")
 // console.log(staticspath)
-const changecspath= path.join(__dirname,"../server/components")
-console.log(changecspath)
+const changecspath = path.join(__dirname, "../server/components");
+console.log(changecspath);
 
-const partialspath= path.join(__dirname,"../server/layout")
-console.log(partialspath)
+const partialspath = path.join(__dirname, "../server/layout");
+console.log(partialspath);
 
-app.set('view engine',"hbs")
-app.set("views", changecspath)
+app.set("view engine", "hbs");
+app.set("views", changecspath);
 
-hbs.registerPartials(partialspath)
-app.get("/",(req,res)=>{
-    res.render("index.hbs",{
-        name:"asim",
-    })
-})
+hbs.registerPartials(partialspath);
+app.get("/", (req, res) => {
+  res.render("index.hbs", {
+    name: "asim",
+  });
+});
 
-app.get("/about",(req,res)=>{
-    res.render("about.hbs",{
-        status:"logout"
-    })
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    status: "logout",
+  });
+});
 
+app.get("/contacts", (req, res) => {
+  res.render("contacts", {
+    status: "logout",
+  });
+});
 
-
-})
-
-
-
-
-app.get("/contacts",(req,res)=>{
-    res.render("contacts",{
-        status:"logout"
-    })
-})
-
-
-app.listen(port,()=>{
-    console.log("connections success")
-})
+app.listen(port, () => {
+  console.log("connections success");
+});
